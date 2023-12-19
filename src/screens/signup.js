659860474f7d18ef8dd55b0f0e0ad3733d687272
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2023-12-19 15:44:36 +0300

    Update sign up screen

--- src/screens/signup.js
+++ src/screens/signup.js
@@ -1,28 +1,52 @@
 const { SafeAreaView, Text, View, StyleSheet, Dimensions, TouchableOpacity, Image} = require("react-native")
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import { showMessage } from "react-native-flash-message"
 import Input from '../components/input'
 import Button from '../components/button'
 import { useTheme } from '@react-navigation/native'
 const { width } = Dimensions.get('window')
+import { AuthContext }  from "../auth/authentication"
+import { signUpApi, singUpApi } from "../lib/apiConnection"
+
 
 const SignUp = ({navigation}) => {
   const [name, setName] = useState('')
-  const [surname, setSurname] = useState('')
+  const [age, setAge] = useState(0)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  
+
   const { colors } = useTheme()
+  const { signIn } = useContext(AuthContext)
 
   const handlerSignUp = () => {
-    if(name === '' || surname === '' || email === '' || password === '') {
+    if(name === '' || age === 0 || email === '' || password === '') {
       showMessage({
         message: "Please fill all the fields",
         type: "info",
         icon: 'info',
         backgroundColor: colors.necessary,
       });
-      return
+    } else {
+      if(!Number(age)) {
+        showMessage({
+          message: "Age must be a number",
+          type: "info",
+          icon: 'info',
+          backgroundColor: colors.necessary,
+        });
+      } else {
+        signUpApi({name, age: Number(age), email, password}).then((res) => {
+          if(!res.error) {
+            signIn({email: email, token: "res.token"})
+            return
+          }
+          showMessage({
+            message: res.error,
+            type: "info",
+            backgroundColor: colors.buttonBackground,
+          });
+        })
+      }
     }
   }
   return (
@@ -30,10 +54,10 @@ const SignUp = ({navigation}) => {
       <View style={styles.imgContainer(colors)}>
         <Image source={require('../../assets/images/rg-img.png')} style={{width: width * 0.6, height: width * 0.6}}/>
       </View>
-      <Input placeholder="Name" iconName={'md-person'} onChangeText={setEmail}/>
+      <Input placeholder="Name" iconName={'md-person'} onChangeText={setName}/>
       <Input placeholder="Email" iconName={'md-mail'} onChangeText={setEmail} type={"email-address"}/>
       <Input placeholder="Password" iconName={'ios-lock-closed'} secureTextEntry={true} onChangeText={setPassword} />
-      <Input placeholder="Age" iconName={'md-body'} onChangeText={setPassword} type={"numeric"}/>
+      <Input placeholder="Age" iconName={'md-body'} onChangeText={setAge} type={"numeric"}/>
       <Button onPress={handlerSignUp}>
         <Text style={styles.buttonText({width})}>Sign up</Text>
       </Button>
