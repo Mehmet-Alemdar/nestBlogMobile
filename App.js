e6e7afe9a7e086c2b6d9ecfa1edd27b69acf98c6
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2023-12-19 14:29:44 +0300

    Add flash message in app.js

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import Authentication from "./src/auth/authentication";
-
+import FlashMessage from "react-native-flash-message";
 const App = () => {
   return (
     <>
       <Authentication />
+      <FlashMessage position="top" />
     </>
   )
 }
