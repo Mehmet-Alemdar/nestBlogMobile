d4a2257acb23d5b499a034c89dd8c179668e211e
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2023-12-19 15:44:04 +0300

    Create sign up api connection

--- src/lib/apiConnection.js
+++ src/lib/apiConnection.js
@@ -6,6 +6,15 @@ export const singInApi = async (body) => {
     const { data } = await axios.post(`${BASE_URL}/user/login`, body)
     return data
   } catch(error) {
-    return {message: 'sign in failed', error}
+    return {message: 'sign in failed', description: error.response.data.message[0], error}
+  }
+}
+
+export const signUpApi = async (body) => {
+  try {
+    const { data } = await axios.post(`${BASE_URL}/user`, body)
+    return data
+  } catch(error) {
+    return {message: 'sign up failed', error}
   }
 }
