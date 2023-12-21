31a749286278986f24d9de23f3b66998b212bca4
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2023-12-22 01:25:42 +0300

    Add detail blog and like blog api

--- src/lib/apiConnection.js
+++ src/lib/apiConnection.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { BASE_URL } from '@env'
+import {getToken} from '../auth/authentication'
 
 export const singInApi = async (body) => {
   try {
@@ -29,5 +30,27 @@ export const fetchBlogPosts = async (token) => {
     return data
   } catch(error) {
     return {message: 'sign in failed', description: error.response.data.message[0], error}
+  }
+}
+
+export const fetchBlogById = async (token,id) => {
+  try {
+    const { data } = await axios.get(`${BASE_URL}/blog/findOne/${id}`, {
+      headers: { Authorization: token}
+    })
+    return data
+  } catch(error) {
+    return {message: 'blog fetching failed', description: error.response.data.message[0], error}
+  }
+}
+
+export const likeBlog = async (token, blogId, userId) => {
+  try {
+    const { data } = await axios.patch(`${BASE_URL}/blog/likeOne/${blogId}/${userId}`, {
+      headers: { Authorization: token}
+    })
+    return data
+  } catch(error) {
+    return {message: 'blog fetching failed', description: error.response.data.message[0], error}
   }
 }
