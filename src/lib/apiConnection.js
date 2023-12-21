86f4a82dbe7f1e658f677f70327b25ef7ff3fb99
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2023-12-21 14:47:39 +0300

    Fetch blogs from api

--- src/lib/apiConnection.js
+++ src/lib/apiConnection.js
@@ -4,6 +4,7 @@ import { BASE_URL } from '@env'
 export const singInApi = async (body) => {
   try {
     const { data } = await axios.post(`${BASE_URL}/user/login`, body)
+    console.log(data);
     return data
   } catch(error) {
     return {message: 'sign in failed', description: error.response.data.message[0], error}
@@ -13,8 +14,20 @@ export const singInApi = async (body) => {
 export const signUpApi = async (body) => {
   try {
     const { data } = await axios.post(`${BASE_URL}/user`, body)
+    console.log(data);
     return data
   } catch(error) {
     return {message: 'sign up failed', error}
+  }
+}
+
+export const fetchBlogPosts = async (token) => {
+  try {
+    const { data } = await axios.get(`${BASE_URL}/blog/findAll`, {
+      headers: { Authorization: token}
+    })
+    return data
+  } catch(error) {
+    return {message: 'sign in failed', description: error.response.data.message[0], error}
   }
 }
